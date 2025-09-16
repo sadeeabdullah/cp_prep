cdbdb4e145f81bde8e4b8aa1bcbc0c45fb01d72b
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-16 23:13:45 +0600

    added test comment

--- codeforces problem beginner level/C_Target_Practice.cpp
+++ codeforces problem beginner level/C_Target_Practice.cpp
@@ -50,7 +50,7 @@ int main()
                 }
         
             
-                
+                // test comment
             }
             int total = (level1 * 1) + (level2 * 2) + (level3 * 3) + (level4 * 4) + (level5 * 5);
 
