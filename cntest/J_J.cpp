2c106854d0124165548c93c1059174463133710e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-30 20:33:24 +0600

    added cmtn

---
+++ cntest/J_J.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    
+    int n, k;
+    cin >> n >> k;
+    int op[k];
+    for(int i = 0 ; i < k; i++)
+    {
+        cin >> op[i];
+    }
+
+    vector<bool> dp(n + 1, false);
+    dp[0] = false;
+
+    for (int i = 1; i<= n; i++)
+    {
+        for(int j = 0; j< k ;j++)
+        {
+            if(op[i] <= i && !dp[i - op[j]])
+            {
+                dp[i] = true;
+                break;// cmt
+            }
+        }
+    }
+
+    for (int i = 1; i <=n; i++)
+    {
+        cout << (dp[i] ? 'W' : 'L');
+    }
+    cout << nl;
+    return 0;
+}
