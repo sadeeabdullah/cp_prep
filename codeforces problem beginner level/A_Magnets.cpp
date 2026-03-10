e0783417ba03d2ed78cefb02bb7c4acba1742bc3
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-03-11 05:23:52 +0600

    added few more problem solution

---
+++ codeforces problem beginner level/A_Magnets.cpp
@@ -0,0 +1,54 @@
+		
+/* Bismillah hir rahmanir rahim */
+
+#include <bits/stdc++.h>
+
+//for pbds
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define rall(var) (var).rbegin(), (var).rend()
+#define all(var) (var).begin(), (var).end()
+#define pb(var) push_back(var)
+const ll Mod = 1e9 + 7;
+
+void Puzzle_Out()
+{
+    int n;	cin>> n;
+    vector<string> st;
+    for(int i = 0; i < n; i++){
+    	string s;	cin >>s ;
+    	st.pb(s);
+    }
+
+
+    int i =0, cnt = 0;
+    while (i < n){
+    	while(st[i] == st[i + 1]){
+    		i++;
+    	}
+    	i++;
+    	cnt++;
+    }
+    cout << cnt << nl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+        Puzzle_Out();
+
+    return 0;
+}
+/* author : Sadee Abdullah */ 
